2afa1631b720f6924cb59bbfd21b935d1e91fe48
milzar <realmilanxavier@gmail.com>
2020-10-07 20:00:01 +0530

    adds "no u", "question" feat

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require("dotenv").config()
 const twss = require('twss');
-const Discord = require("discord.js")
-const client = new Discord.Client()
-const TWSS_RESPONSE = "That's what she said!"
-const THRESHOLD = process.env.THRESHOLD
-const MIN_WORD_COUNT = process.env.MIN_WORD_COUNT
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const  googleIt = require('google-it')
+
+const TWSS_RESPONSE = "That's what she said!";
+const NO_U = "no u";
+const THRESHOLD = process.env.THRESHOLD;
+const MIN_WORD_COUNT = process.env.MIN_WORD_COUNT;
+const QUESTION = "question";
 
 
 
@@ -14,14 +18,36 @@ client.on("ready", () => {
 })
 
 client.on("message", (msg) => {
-const message = msg.content
+const message = msg.content.trim().toLowerCase()
 
 // Ignore bot messages and short sentences
 if(msg.author.bot || message.split(' ').length < MIN_WORD_COUNT ){
   return;
 }
 
-console.log( message, " probablitiy is ", twss.prob(message))
+// QUESTION
+if(message.startsWith(QUESTION)){
+  const query = message.slice(message.indexOf(QUESTION) + QUESTION.length + 1 );
+  googleIt({'query': query}).then(results => {
+      if(results && results.length){
+          msg.reply(results[0].title + "\n" + results[0].link)
+      }
+      else{
+        msg.reply('idk. Ask someone else');
+      }
+
+  }).catch(e => {
+    console.log("error while googling for", query, e)
+  })
+  return;
+}
+
+// Respond to Mentions
+if(msg.isMemberMentioned(client.user)){
+  msg.reply(NO_U)
+  return;
+}
+
 
 if (twss.prob(message) >= THRESHOLD) {
     msg.reply(TWSS_RESPONSE)
