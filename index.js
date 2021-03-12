0cd93569aeeb5572a124ffed5ec80663759b5f23
Milan Xavier <realmilanxavier@gmail.com>
2021-03-12 13:05:33 +0530

    react to messages with SHE instead of replying

--- index.js
+++ index.js
@@ -11,46 +11,47 @@ const MIN_WORD_COUNT = process.env.MIN_WORD_COUNT;
 const QUESTION = "question";
 
 
-
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-  console.log("Running with Threshold: ", THRESHOLD, " MIN_WORD_COUNT: ", MIN_WORD_COUNT)
+    console.log(`Logged in as ${client.user.tag}!`)
+    console.log("Running with Threshold: ", THRESHOLD, " MIN_WORD_COUNT: ", MIN_WORD_COUNT)
 })
 
 client.on("message", (msg) => {
-  const message = msg.content.trim().toLowerCase()
+    const message = msg.content.trim().toLowerCase()
 
-  // Ignore bot messages and short sentences
-  if(msg.author.bot || message.split(' ').length < MIN_WORD_COUNT ){
-    return;
-}
+    // Ignore bot messages and short sentences
+    if (msg.author.bot || message.split(' ').length < MIN_WORD_COUNT) {
+        return;
+    }
 
 // QUESTION
-if(message.startsWith(QUESTION)){
-  const query = message.slice(message.indexOf(QUESTION) + QUESTION.length + 1 );
-  googleIt({'query': query}).then(results => {
-      if(results && results.length){
-          msg.reply(results[0].title + "\n" + results[0].link)
-      }
-      else{
-        msg.reply('idk. Ask someone else');
-      }
+    if (message.startsWith(QUESTION)) {
+        const query = message.slice(message.indexOf(QUESTION) + QUESTION.length + 1);
+        googleIt({'query': query}).then(results => {
+            if (results && results.length) {
+                msg.reply(results[0].title + "\n" + results[0].link)
+            } else {
+                msg.reply('idk. Ask someone else');
+            }
 
-  }).catch(e => {
-      console.log("error while googling for", query, e)
-  })
-  return;
-}
+        }).catch(e => {
+            console.log("error while googling for", query, e)
+        })
+        return;
+    }
 
 // Respond to Mentions
-if(msg.isMemberMentioned(client.user)){
-  msg.reply(NO_U)
-  return;
-}
+    if (msg.isMemberMentioned(client.user)) {
+        msg.reply(NO_U)
+        return;
+    }
 
 // TWSS responses
-if (twss.prob(message) >= THRESHOLD && Math.random() > 0.7 ) {
-      msg.reply(TWSS_RESPONSE)
+    if (twss.prob(message) >= THRESHOLD) {
+        msg.react("regional_indicator_s");
+        msg.react("regional_indicator_h");
+        msg.react("regional_indicator_e");
+
     }
 })
 
